5c4e16cb565eb653609a5f47d536347265ce10a7
adolphlwq <kenan3015@gmail.com>
2016-12-14 16:57:35 +0800

    fix issue-2
    Signed-off-by: adolphlwq <kenan3015@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,27 @@
 #! /bin/bash
 set -x
+
+# change sshd config
 if [ -z $SSH_PORT ]; then
   export  SSH_PORT=2221
 fi
-
 port_line=`grep "Port " /etc/ssh/sshd_config`
 sed -i "s/$port_line/Port $SSH_PORT/g" /etc/ssh/sshd_config
 sed -i "35s/.*/    StrictHostKeyChecking no/g" /etc/ssh/ssh_config
 service ssh start
 
+# set hdfs url in core-site.xml
+if [ -z $HDFSURL ]; then
+    export HDFSURL=localhost
+fi
+sed "6s@.*@      <value>hdfs://${HDFSURL}:9000</value>@" -i  $HADOOP_HOME/etc/hadoop/core-site.xml
+
+# set passwdless
 ssh-keygen -t rsa -q -P '' -f /root/.ssh/id_rsa
 cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
 
 $HADOOP_HOME/etc/hadoop/hadoop-env.sh
-echo "Y" | $HADOOP_HOME/bin/hdfs namenode -format
+echo "N" | $HADOOP_HOME/bin/hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 
 # https://github.com/sequenceiq/hadoop-docker/blob/master/bootstrap.sh#L21-L27
